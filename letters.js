648c34fc809f99c4aaf6b214f6be9b5a14a5fc8f
mengyao-xiu <55821486+mengyao-xiu@users.noreply.github.com>
2020-02-07 23:18:58 -0800

    Create letters.js

---
+++ letters.js
@@ -0,0 +1,231 @@
+function setup() {
+  createCanvas(1000, 1000);
+}
+
+function draw() {
+  background(200);
+  printA(50,50)
+  printB(80,50)
+  printC(130,50)
+  printD(180,50)
+  printE(230,50)
+  printF(280,50)
+  printG(330,50)
+  printH(380,50)
+  printI(430,50)
+  printJ(480,50)
+  printK(530,50)
+  printL(580,50)
+  printM(630,50)
+  printN(680,50)
+  printO(730,50)
+  printP(780,50)
+  printQ(830,50)
+  printR(880,50)
+  printS(50,200)
+  printT(100,200)
+  printU(150,200)
+  printV(200,200)
+  printW(250,200)
+  printX(300,200)
+  printY(350,200)
+  printZ(400,200)
+  
+}
+
+  
+function printA(x,y){
+  //Draw an A
+  line(x,y,x-25,y+100);
+  line(x,y,x+25,y+100);
+  line(x-12,x+50,x+12,x+50);
+}
+
+function printB(x,y){
+  //Draw a B
+  line(x,y,x,y+100);
+  line(x,y,x+40,y);
+  line(x,y+50,x+25,y+50);
+  line(x,y+100,x+40,y+100);
+  line(x+40,y,x+25,y+50);
+  line(x+25,y+50,x+40,y+100);
+}
+
+
+function printC(x,y){
+  // Draw a C
+  line(x,y,x+40,y);
+  line(x,y,x,y+100);
+  line(x,y+100,x+40,y+100);
+}
+
+function printD(x,y){
+  // Draw a D
+  line(x,y,x+40,y);
+  line(x,y,x,y+100);
+  line(x,y+100,x+25,y+100);
+  line(x+40,y,x+25,y+100);
+}
+
+function printE(x,y){
+  //Draw an E
+  line(x,y,x+40,y);
+  line(x,y,x,y+100);
+  line(x,y+100,x+40,y+100);
+  line(x,y+50,x+25,y+50);
+}
+
+function printF(x,y){
+  //Draw a F
+  line(x,y,x+40,y);
+  line(x,y,x,y+100);
+  line(x,y+50,x+25,y+50);
+}
+
+function printG(x,y){
+  //Draw a G
+  line(x,y,x+40,y);
+  line(x,y,x,y+100);
+  line(x,y+100,x+40,y+100);
+  line(x+40,y+70,x+40,y+100);
+  line(x+25,y+70,x+40,y+70);
+}
+
+function printH(x,y){
+  //Draw a H
+  line(x,y,x,y+100);
+  line(x,y+50,x+40,y+50);
+  line(x+40,y,x+40,y+100);
+}
+
+function printI(x,y){
+  //Draw an I
+  line(x,y,x+40,y);
+  line(x+20,y,x+20,y+100);
+  line(x,y+100,x+40,y+100);
+}
+
+function printJ(x,y){
+  //Draw a J
+  line(x,y,x+40,y);
+  line(x+20,y,x+20,y+100);
+  line(x+2,y+100,x+20,y+100);
+}
+
+function printK(x,y){
+  //Draw a K
+  line(x,y,x,y+100);
+  line(x+40,y,x,y+50);
+  line(x,y+50,x+40,y+100);
+}
+
+function printL(x,y){
+  //Draw a L
+  line(x,y,x,y+100);
+  line(x,y+100,x+40,y+100);
+}
+
+function printM(x,y){
+  //Draw a M
+  line(x,y,x,y+100);
+  line(x,y,x+20,y+40);
+  line(x+40,y,x+20,y+40);
+  line(x+40,y,x+40,y+100);
+}
+
+function printN(x,y){
+  //Draw a N
+  line(x,y,x,y+100);
+  line(x,y,x+40,y+100);
+  line(x+40,y,x+40,y+100);
+}
+
+function printO(x,y){
+  //Draw an O
+  line(x,y,x+40,y);
+  line(x,y,x,y+100);
+  line(x+40,y,x+40,y+100);
+  line(x,y+100,x+40,y+100);
+}
+
+function printP(x,y){
+  //Draw a P
+  line(x,y,x,y+100);
+  line(x,y,x+40,y);
+  line(x+40,y,x+40,y+40);
+  line(x,y+40,x+40,y+40);
+}
+
+function printQ(x,y){
+  //Draw a Q
+  line(x,y,x,y+100);
+  line(x,y,x+40,y);
+  line(x+40,y,x+40,y+100);
+  line(x,y+100,x+40,y+100);
+  line(x+20,y+70,x+42,y+100);
+}
+
+function printR(x,y){
+  //Draw a R
+  line(x,y,x,y+100);
+  line(x,y,x+40,y);
+  line(x+40,y,x+40,y+40);
+  line(x,y+40,x+40,y+40);
+  line(x,y+40,x+40,y+100);
+}
+
+function printS(x,y){
+  //Draw a S
+  line(x,y,x+40,y);
+  line(x,y,x,y+50);
+  line(x,y+50,x+40,y+50);
+  line(x+40,y+50,x+40,y+100);
+  line(x,y+100,x+40,y+100);
+}
+
+function printT(x,y){
+  //Draw a T
+  line(x,y,x+40,y);
+  line(x+20,y,x+20,y+100);
+}
+
+function printU(x,y){
+  //Draw an U
+  line(x,y,x,y+100);
+  line(x,y+100,x+40,y+100);
+  line(x+40,y,x+40,y+100);
+}
+
+function printV(x,y){
+  //Draw a V
+  line(x,y,x+20,y+100);
+  line(x+40,y,x+20,y+100);
+}
+
+function printW(x,y){
+  //Draw a W
+  line(x,y,x+15,y+100);
+  line(x+15,y+100,x+20,y+20);
+  line(x+20,y+20,x+35,y+100);
+  line(x+35,y+100,x+40,y);
+}
+
+function printX(x,y){
+  //Draw a X
+  line(x,y,x+40,y+100);
+  line(x+40,y,x,y+100);
+}
+
+function printY(x,y){
+  //Draw a Y
+  line(x,y,x+20,y+40);
+  line(x+20,y+40,x+40,y);
+  line(x+20,y+40,x+20,y+100);
+}
+
+function printZ(x,y){
+  //Draw a Z
+  line(x,y,x+40,y);
+  line(x+40,y,x,y+100);
+  line(x,y+100,x+40,y+100);
+}
